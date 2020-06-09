cba29372541d80fd7f935159e1b97aeffad15aba
unknown <ch.kanoy@gmail.com>
2020-06-08 19:34:24 -0500

    first commit

---
+++ timer.py
@@ -0,0 +1,9 @@
+# time a program and submit it to a permanent log for later access
+def main():
+    project = input("Hello! What project are you working on today?")
+    # if project doesn't exist in database, prompt the creation of a new one or go back
+
+    print("Have fun working on " + project)
+
+if __name__ == "__main__":
+    main()
