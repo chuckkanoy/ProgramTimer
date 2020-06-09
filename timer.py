4fd6c2aaf186de285059775938a4eefabac6d8ea
unknown <ch.kanoy@gmail.com>
2020-06-08 19:37:50 -0500

    added basic skeleton

--- timer.py
+++ timer.py
@@ -2,6 +2,7 @@
 def main():
     project = input("Hello! What project are you working on today?")
     # if project doesn't exist in database, prompt the creation of a new one or go back
+    # if project does exist, access it's file and 
 
     print("Have fun working on " + project)
 
