b081b397601cc523c250e7a4cd86c5a12d1ae6de
unknown <ch.kanoy@gmail.com>
2020-06-12 19:14:59 -0500

    got writing to a file kind of working

--- timer.py
+++ timer.py
@@ -1,4 +1,6 @@
 from os import system, name
+import os
+import time
 from time import sleep
 import threading
 import sys
@@ -30,30 +32,41 @@ def clear():
 # display message based on random
 def getMessage():
     messages = ['Another day, another dollar.', 'Great work today! Come back tomorrow (or don\'t, I\'m just a program)', 'Proud of you :)']
-    print(messages[random.randint(0, len(messages))])
+    print(messages[random.randint(0, len(messages) - 1)])
 
 # time a program and submit it to a permanent log for later access
 def main():
     project = input("Hello! What project are you working on today?\n")
     flag = True
+    storage = []
+    index = 0
+    count = 0
 
     # if project doesn't exist in database, prompt the creation of a new one or go back
     f = open('records.txt', 'r')
     for line in f:
-        if line.split()[1] == project:
-            flag = False
-    f.close()
-        
+        # check to see if the size of the file is zero
+        if os.path.getsize("records.txt") != 0:
+            storage.append(line)
+            print(line.split(',')[0])
+            if line.split(',')[0] == project:
+                index = count
+                flag = False
+            count = count + 1
+        else:
+            flag = True
+    
+    # handle when no record is found
     if(flag):
         response = input(project + 'not found, would you like to create it?(Y/N)\n')
-
-    if response.lower == 'n':
-        print('Understandable, have a nice day')
-        sys.exit()
+        if response.lower == 'n':
+            print('Understandable, have a nice day')
+            sys.exit()
+        storage.append(project + "," + str(time.timedelta(0, 0, 0)))
 
     # if project does exist, access it's file and 
     
-
+    print(str(datetime.timedelta(0,0,0)))
     # create thread for animation
     x = threading.Thread(target=clear, daemon=True)
     x.start()
@@ -67,10 +80,16 @@ def main():
             end_time = datetime.datetime.now()
             finishing_time = end_time - start_time
             print('Worked for: ' + str(finishing_time))
-            # f = open("records.txt", "w")
-            # for line in f:
-            #     if line.split()[1] == project:
+            f = open("records.txt", "w")
 
+            # write the new data to the file
+            for line in storage:
+                print(line.split(','))
+                # modify old value and write it to file
+                if line.split(',')[0] == project:
+                    line = project + "," + str(finishing_time + time.strptime(int(line.split(',')[1]), "%H:%M:%S").replace("\n","")) + "\n"
+                f.write(line)
+            f.close()
             sys.exit()
 
 if __name__ == "__main__":
